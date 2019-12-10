26932f7db070499c150a63bbfa263f382cc1f415
Prasanna Gopalakrishnan <prasanna.gopal@rea-group.com>
2019-12-10 17:42:45 +1100

    Pullback with get and set

--- ComposableArchitectureSinglePage/ContentView.swift
+++ ComposableArchitectureSinglePage/ContentView.swift
@@ -114,7 +114,23 @@ func favoritePrimesReducer(state: inout AppState, action: AppAction) {
   }
 }
 
-let appReducer = combine(counterReducer,
+func pullback<LocalValue, GlobalValue, Action>(
+  _ reducer: @escaping (inout LocalValue, Action) -> Void,
+  get: @escaping (GlobalValue) -> LocalValue,
+  set: @escaping (inout GlobalValue, LocalValue) -> Void
+) -> (inout GlobalValue, Action) -> Void {
+  
+  return { globalValue, action in
+    var localValue = get(globalValue)
+    reducer(&localValue, action)
+    set(&globalValue, localValue)
+  }
+  
+}
+
+let appReducer = combine(pullback(counterReducer,
+                                  get: { $0.count },
+                                  set: { $0.count = $1 } ),
                          primeModalReducer,
                          favoritePrimesReducer)
 
