dc9d345aab122f6eeb9e8f214a8dbf284fefa1d0
Prasanna Gopalakrishnan <prasanna.gopal@rea-group.com>
2019-12-28 18:08:22 +1100

    Moved all the architectural pieces together

--- ComposableArchitectureSinglePage/ContentView.swift
+++ ComposableArchitectureSinglePage/ContentView.swift
@@ -168,6 +168,20 @@ func favoritePrimesReducer(state: inout [Int], action: FavoritePrimesAction) {
   }
 }
 
+final class Store<Value, Action>: ObservableObject {
+  let reducer: (inout Value, Action) -> Void
+  @Published private(set) var value: Value
+  
+  init(initialValue: Value, reducer: @escaping (inout Value, Action) -> Void) {
+    self.reducer = reducer
+    self.value = initialValue
+  }
+  
+  func send(_ action: Action) {
+    self.reducer(&self.value, action)
+  }
+}
+
 func pullback<LocalValue, GlobalValue, LocalAction, GlobalAction>(
   _ reducer: @escaping (inout LocalValue, LocalAction) -> Void,
   value: WritableKeyPath<GlobalValue, LocalValue>,
@@ -177,6 +191,28 @@ func pullback<LocalValue, GlobalValue, LocalAction, GlobalAction>(
   return { globalValue, globalAction in
     guard let localAction = globalAction[keyPath: action] else { return }
     reducer(&globalValue[keyPath: value], localAction)
+  }
+}
+
+func combine<Value, Action>(
+  _ reducers: (inout Value, Action) -> Void...)
+  -> (inout Value, Action) -> Void {
+    return { value, action in
+      for reducer in reducers {
+        reducer(&value, action)
+      }
+    }
+}
+
+func logging<Value, Action> (
+  _ reducer: @escaping (inout Value, Action) -> Void
+) -> (inout Value, Action) -> Void {
+  return { value, action in
+    reducer(&value, action)
+    print("Action: \(action)")
+    print("Value: ")
+    dump(value)
+    print("---")
   }
 }
 
@@ -213,43 +249,6 @@ let appReducer: (inout AppState, AppAction) -> Void = combine(
   pullback(primeModalReducer, value: \.self, action: \.primeModal),
   pullback(favoritePrimesReducer, value: \.favoritePrimes, action: \.favoritePrimes)
 )
-
-func combine<Value, Action>(
-  _ reducers: (inout Value, Action) -> Void...)
-  -> (inout Value, Action) -> Void {
-    return { value, action in
-      for reducer in reducers {
-        reducer(&value, action)
-      }
-    }
-}
-
-func logging<Value, Action> (
-  _ reducer: @escaping (inout Value, Action) -> Void
-) -> (inout Value, Action) -> Void {
-  return { value, action in
-    reducer(&value, action)
-    print("Action: \(action)")
-    print("Value: ")
-    dump(value)
-    print("---")
-  }
-}
-  
-final class Store<Value, Action>: ObservableObject {
-  let reducer: (inout Value, Action) -> Void
-  @Published var value: Value
-  
-  init(initialValue: Value, reducer: @escaping (inout Value, Action) -> Void) {
-    self.reducer = reducer
-    self.value = initialValue
-  }
-  
-  func send(_ action: Action) {
-    self.reducer(&self.value, action)
-  }
-}
-
 
 struct PrimeAlert: Identifiable {
   let prime: Int
