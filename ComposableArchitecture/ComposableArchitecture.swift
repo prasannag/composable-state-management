1e4d78dfa7ec708552c872f2efc35ee3e20c4cad
Prasanna Gopalakrishnan <prasanna.gopal@rea-group.com>
2019-12-29 03:02:43 +1100

    Added view function to Store that returns a store that operates on localValue from globalValue

--- ComposableArchitecture/ComposableArchitecture.swift
+++ ComposableArchitecture/ComposableArchitecture.swift
@@ -8,6 +8,7 @@ import Combine
 public final class Store<Value, Action>: ObservableObject {
   private let reducer: (inout Value, Action) -> Void
   @Published public private(set) var value: Value
+  private var cancellable: Cancellable?
   
   public init(initialValue: Value, reducer: @escaping (inout Value, Action) -> Void) {
     self.reducer = reducer
@@ -16,6 +17,22 @@ public final class Store<Value, Action>: ObservableObject {
   
   public func send(_ action: Action) {
     self.reducer(&self.value, action)
+  }
+  
+  public func view<LocalValue>(
+    _ f: @escaping (Value) -> LocalValue
+  ) -> Store<LocalValue, Action> {
+    let localStore = Store<LocalValue, Action>(
+      initialValue: f(self.value),
+      reducer: { localValue, action in
+        self.send(action)
+        localValue = f(self.value)
+      }
+    )
+    localStore.cancellable = self.$value.sink { [weak localStore] newValue in
+      localStore?.value = f(newValue)
+    }
+    return localStore
   }
 }
 
