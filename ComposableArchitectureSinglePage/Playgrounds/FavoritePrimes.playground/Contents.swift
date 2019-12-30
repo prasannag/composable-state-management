b6b9eea9100b4ecb6bfcdee72c3bea78cd7e67a1
Prasanna Gopalakrishnan <prasanna.gopal@rea-group.com>
2019-12-30 16:49:14 +1100

    Created FavoritePriems playground

--- ComposableArchitectureSinglePage/Playgrounds/FavoritePrimes.playground/Contents.swift
+++ ComposableArchitectureSinglePage/Playgrounds/FavoritePrimes.playground/Contents.swift
@@ -1,28 +1,8 @@
 import UIKit
 import ComposableArchitecture
-//import Counter
 import FavoritePrimes
-//import PrimeModal
-import SwiftUI
 import PlaygroundSupport
-
-//PlaygroundPage.current.liveView = UIHostingController(
-//  rootView: CounterView(
-//    store: Store<CounterViewState, CounterViewAction>(
-//      initialValue: (10000000, [2, 5, 7]),
-//      reducer: counterViewReducer
-//    )
-//  )
-//)
-
-//PlaygroundPage.current.liveView = UIHostingController(
-//  rootView: IsPrimeModalView(
-//    store: Store<PrimeModalState, PrimeModalAction>(
-//      initialValue: (3, [2, 5, 7]),
-//      reducer: primeModalReducer
-//    )
-//  )
-//)
+import SwiftUI
 
 PlaygroundPage.current.liveView = UIHostingController(
   rootView: NavigationView {
@@ -34,30 +14,3 @@ PlaygroundPage.current.liveView = UIHostingController(
     )
   }
 )
-
-//let store = Store<Int, Void>(initialValue: 0, reducer: { count, _ in count += 1 })
-//
-//store.send(())
-//store.send(())
-//store.send(())
-//store.send(())
-//store.send(())
-//store.send(())
-//
-//store.value
-//
-//let newStore = store.view { $0 }
-//newStore.value
-//
-//newStore.send(())
-//newStore.send(())
-//newStore.send(())
-//newStore.value
-//store.value
-//
-//store.send(())
-//store.send(())
-//store.send(())
-//store.send(())
-//store.value
-//newStore.value
